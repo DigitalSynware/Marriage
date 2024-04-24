1b0a7954970c233a9f9fbed9f4a172ec7f289633
samfces <samuel@digitalsynware.com>
2024-04-24 08:14:42 +0200

    feat: help command format changed

--- src/main/java/com/lenis0012/bukkit/marriage2/commands/CommandHelp.java
+++ src/main/java/com/lenis0012/bukkit/marriage2/commands/CommandHelp.java
@@ -23,37 +23,40 @@ public class CommandHelp extends Command {
     @Override
     public void execute() {
         MarriageCommandExecutor commandExecutor = ((MarriageBase) marriage).getCommandExecutor();
-        reply("Version: &a" + marriage.getPlugin().getDescription().getVersion());
-        reply("&2&m---------&2< &a&lMarriage Command Help &2>&2&m---------"); // Play around with the amount of dashes later
+        reply("");
+        reply("Ⓨ &aAyuda de Comandos &7- &a&lMatrimonios");
+        reply("");
         for(Command command : commandExecutor.getSubCommands()) {
             if(command.isHidden()) {
                 continue;
             }
 
             String alias = command instanceof CommandMarry ? "" : command.getAliases()[0] + " ";
-            String text = "&a/marry " + alias + command.getUsage() + " &f- &7" + command.getDescription();
+            String text = "&7 • &e/marry " + alias + command.getUsage() + " &f- &7" + command.getDescription();
             if(command.getExecutionFee() == 0.0 || !marriage.dependencies().isEconomyEnabled() || player == null) {
                 reply(text);
                 continue;
             }
             ComponentBuilder builder = new ComponentBuilder("/marry " + alias + command.getUsage()).color(ChatColor.GREEN)
-                    .event(new HoverEvent(Action.SHOW_TEXT, new ComponentBuilder("Cost: "
+                    .event(new HoverEvent(Action.SHOW_TEXT, new ComponentBuilder("Coste: "
                             + marriage.dependencies().getEconomyService().format(command.getExecutionFee())).create()))
                     .append(" - ").color(ChatColor.WHITE)
-                    .event(new HoverEvent(Action.SHOW_TEXT, new ComponentBuilder("Cost: "
+                    .event(new HoverEvent(Action.SHOW_TEXT, new ComponentBuilder("Coste: "
                             + marriage.dependencies().getEconomyService().format(command.getExecutionFee())).create()))
                     .append(command.getDescription()).color(ChatColor.GRAY)
-                    .event(new HoverEvent(Action.SHOW_TEXT, new ComponentBuilder("Cost: "
+                    .event(new HoverEvent(Action.SHOW_TEXT, new ComponentBuilder("Coste: "
                             + marriage.dependencies().getEconomyService().format(command.getExecutionFee())).create()));
             player.spigot().sendMessage(builder.create());
         }
 
+        reply("");
+        reply("§7Tu estado actual:");
         String status = Message.SINGLE.toString();
         if(player != null && player.hasMetadata("marriedTo")) {
             String partner = player.getMetadata("marriedTo").get(0).asString();
             status = String.format(Message.MARRIED_TO.toString(), partner);
         }
         reply(Message.STATUS, status);
-        reply("&2&m--------------------------------------------"); // Play around with the amount of dashes later
+        reply(""); // Play around with the amount of dashes later
     }
 }
